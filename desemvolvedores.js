c4920846c59964617855f088213e9207d88f7fd8
Ricellyo Cruz Silva <ricellyocruzsilva@gmail.com.br>
2024-03-16 10:13:17 -0300

    adicinando aquivo da apresentacao final

--- desemvolvedores.js
+++ desemvolvedores.js
@@ -5,15 +5,15 @@ const info = [
    
     {
         // img: "imgDevs/Avatar.png",
-        img: "imgDevs/Avatar.png",
-        desc: "🔸 Óla, Eu sou Ricellyo Cruz Silva.",
-        texto: "Tecnologo em Analista e Desemvolvedor de Sistemas pela FACAM(Faculdade do Maranhão) . Minha paixão por tecnologia iniciou 2017, Das diversas areas que estudei Mi indentifiquei melhor com desenvolvimento Web (Front-End). Estou no inicio da minha carreira  e tenho como objetivo crescer  e mi tornar um desevolvedor Web Front-End. 🚀",
-        formacao: "Analise e Desenvolvimento de Sistemas | Front-End | GitHub | HTML | CSS ",
+        img: "imgDevs/91381008.png",
+        desc: "💜 Olá, meu nome é Pedro Vitor!",
+        texto: "Formado em Análise e Desenvolvimento de Sistemas, sou apaixonado por tecnologia e estou no início da minha jornada como desenvolvedor, meu objetivo é aprender e crescer na área de desenvolvimento Backend.",
+        formacao: "<strong>Desenvolvedor: </strong> Backend | Node JS | JavaScript | MySQL",
         habilidades: "🚀 Habilidades em Destaque:",
         habilidade1: "Bagds/HTML5-E34F26.svg",
         habilidade2: "Bagds/CSS-239120.svg",
         habilidade3: "Bagds/GIT-E44C30.svg",
-        habilidade4: "Bagds/javascript.svg"
+        habilidade3: "Bagds/TypeScript-007ACC.svg",
     },
      //Segundo dev
 
@@ -38,24 +38,8 @@ const info = [
 
     {
         // img: "imgDevs/Avatar.png",
-        img: "imgDevs/91381008.png",
-        desc: "💜 Olá, meu nome é Pedro Vitor!",
-        texto: "Formado em Análise e Desenvolvimento de Sistemas, sou apaixonado por tecnologia e estou no início da minha jornada como desenvolvedor, meu objetivo é aprender e crescer na área de desenvolvimento Backend.",
-        formacao: "<strong>Desenvolvedor: </strong> Backend | Node JS | JavaScript | MySQL",
-        habilidades: "🚀 Habilidades em Destaque:",
-        habilidade1: "Bagds/HTML5-E34F26.svg",
-        habilidade2: "Bagds/CSS-239120.svg",
-        habilidade3: "Bagds/GIT-E44C30.svg",
-        habilidade3: "Bagds/TypeScript-007ACC.svg",
-    },
-
-    // Quarto dev
-
-   
-    {
-        // img: "imgDevs/Avatar.png",
         img: "imgDevs/152100863.png",
-        desc: "🔸 Óla, Eu sou Ricellyo Cruz Silva.",
+        desc: "🔸 Fernanda Polyanna barancho.",
         texto: "Graduada em Administração. Meu objetivo é ser empreendedora de sucesso e acredito que a tecnologia me ajudará.🚀",
         
         formacao: "Graduada em Administração | Front-End | GitHub | HTML | CSS ",
@@ -65,10 +49,27 @@ const info = [
         habilidade3: "Bagds/GIT-E44C30.svg"
     },
 
-    //  Quinto dev
+    // Quarto dev
+
    
+    {
+        // img: "imgDevs/Avatar.png",
+        img: "imgDevs/Avatar.png",
+        desc: "🔸 Óla, Eu sou Ricellyo Cruz Silva.",
+        texto: "Tecnologo em Analista e Desemvolvedor de Sistemas pela FACAM(Faculdade do Maranhão) . Minha paixão por tecnologia iniciou 2017, Das diversas areas que estudei Mi indentifiquei melhor com desenvolvimento Web (Front-End). Estou no inicio da minha carreira  e tenho como objetivo crescer  e mi tornar um desevolvedor Web Front-End. 🚀",
+        formacao: "Analise e Desenvolvimento de Sistemas | Front-End | GitHub | HTML | CSS ",
+        habilidades: "🚀 Habilidades em Destaque:",
+        habilidade1: "Bagds/HTML5-E34F26.svg",
+        habilidade2: "Bagds/CSS-239120.svg",
+        habilidade3: "Bagds/GIT-E44C30.svg",
+        habilidade4: "Bagds/javascript.svg"
+    },
+
+    //  Quinto dev
 
     
+   
+
 ]
 
 
@@ -121,7 +122,7 @@ div.innerHTML = `<div id="dev">
     <img src=${info[i].habilidade2} alt="">
     <img src=${info[i].habilidade3} alt="">
 </div>
-</div> `
+</div> <br> `
 
 let main = document.querySelector("main");
 main.appendChild(div); 
